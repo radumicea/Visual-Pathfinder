7695d6be730030d099405de95177c7dc5fdd33ab
Radu <69594375+radumicea@users.noreply.github.com>
2022-01-20 12:28:15 +0200

    Update ButtonCreationHandler.cs

--- src/UI Handler/ButtonCreationHandler.cs
+++ src/UI Handler/ButtonCreationHandler.cs
@@ -15,16 +15,12 @@ namespace UIHandlerNamespace
 
         public static void SetButtonsPosition(int vertical, int horizontal)
         {
-            var posRatio = (vertical <= horizontal) ? -0.6F : 0.5F;
+            const float posRatio = -0.6F;
             var dw = 0.15F * horizontal;
-            var aMin = (vertical <= horizontal) ? new Vector2(0, 1) : new Vector2(0, 0);
-            var aMax = (vertical <= horizontal) ? new Vector2(0, 1) : new Vector2(0, 0);
 
             var canvasTrans = GameObject.Find("Canvas").GetComponent<RectTransform>();
 
             var a = canvasTrans.Find("ChooseAlgorithm").GetComponent<RectTransform>();
-            a.anchorMin = aMin;
-            a.anchorMax = aMax;
             if (a.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 dw = 0;
@@ -33,8 +29,6 @@ namespace UIHandlerNamespace
             a.anchoredPosition = new Vector2(0.5F * a.rect.width + dw, posRatio * a.rect.height);
 
             var s = canvasTrans.Find("SetStart").GetComponent<RectTransform>();
-            s.anchorMin = aMin;
-            s.anchorMax = aMax;
             if (s.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 s.sizeDelta = new Vector2(canvasTrans.rect.width / NumButtons, s.rect.height);
@@ -42,8 +36,6 @@ namespace UIHandlerNamespace
             s.anchoredPosition = new Vector2(a.anchoredPosition.x + 0.5F * (a.rect.width + s.rect.width) + dw, posRatio * s.rect.height);
 
             var ips = canvasTrans.Find("SetIPs").GetComponent<RectTransform>();
-            ips.anchorMin = aMin;
-            ips.anchorMax = aMax;
             if (ips.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 ips.sizeDelta = new Vector2(canvasTrans.rect.width / NumButtons, ips.rect.height);
@@ -51,8 +43,6 @@ namespace UIHandlerNamespace
             ips.anchoredPosition = new Vector2(s.anchoredPosition.x + 0.5F * (s.rect.width + ips.rect.width) + dw, posRatio * ips.rect.height);
 
             var dt = canvasTrans.Find("DestroyTiles").GetComponent<RectTransform>();
-            dt.anchorMin = aMin;
-            dt.anchorMax = aMax;
             if (dt.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 dt.sizeDelta = new Vector2(canvasTrans.rect.width / NumButtons, dt.rect.height);
@@ -60,8 +50,6 @@ namespace UIHandlerNamespace
             dt.anchoredPosition = new Vector2(ips.anchoredPosition.x + 0.5F * (ips.rect.width + dt.rect.width) + dw, posRatio * dt.rect.height);
 
             var ss = canvasTrans.Find("SetSpeed").GetComponent<RectTransform>();
-            ss.anchorMin = aMin;
-            ss.anchorMax = aMax;
             if (ss.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 ss.sizeDelta = new Vector2(canvasTrans.rect.width / NumButtons, ss.rect.height);
@@ -69,8 +57,6 @@ namespace UIHandlerNamespace
             ss.anchoredPosition = new Vector2(dt.anchoredPosition.x + 0.5F * (dt.rect.width + ss.rect.width) + dw, posRatio * ss.rect.height);
 
             var sst = canvasTrans.Find("SetSpeedText").GetComponent<RectTransform>();
-            sst.anchorMin = aMin;
-            sst.anchorMax = aMax;
             if (sst.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 sst.sizeDelta = new Vector2(canvasTrans.rect.width / NumButtons, sst.rect.height);
@@ -78,8 +64,6 @@ namespace UIHandlerNamespace
             sst.anchoredPosition = new Vector2(dt.anchoredPosition.x + 0.5F * (dt.rect.width + sst.rect.width) + dw, posRatio * sst.rect.height);
 
             var c = canvasTrans.Find("ChooseClear").GetComponent<RectTransform>();
-            c.anchorMin = aMin;
-            c.anchorMax = aMax;
             if (c.rect.width * NumButtons > canvasTrans.rect.width)
             {
                 c.sizeDelta = new Vector2(canvasTrans.rect.width / NumButtons, c.rect.height);
